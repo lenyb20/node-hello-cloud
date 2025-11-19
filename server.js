57fba4daf8e56598c5049bf7cba8bb2f7d525728
lenyb20 <leny.basilan@gmail.com>
2025-11-19 14:04:57 +0000

    Initial Node-hello cloud

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require("http");
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello bebeb! You are the best husband in the world!");
-  res.end();
+  if (req.url === "/api") {
+    res.writeHead(200, {"Content-Type":"application/json"});
+    res.end(JSON.stringify({ message: "Hello Cloud", author: "Your Name" }));
+  } else {
+    res.writeHead(200, {"Content-Type":"text/plain"});
+    res.end("Hello Cloud from Node.js!");
+  }
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });

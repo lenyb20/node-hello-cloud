083e1c156071ac77d654daca0a12324df9b66501
lenyb20 <leny.basilan@gmail.com>
2025-11-19 14:19:26 +0000

    Update message text

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const http = require("http");
-const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/api") {
-    res.writeHead(200, {"Content-Type":"application/json"});
-    res.end(JSON.stringify({ message: "Hello Cloud", author: "Your Name" }));
-  } else {
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("Hello Cloud from Node.js!");
-  }
+  res.write("Hello Cloud from Node.js!");
+  res.end();
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
